9b3c4b5bd73287838c6ed8860a336f9b5780578e
Rashoo18 <115503306+Rashoo18@users.noreply.github.com>
2024-04-30 21:30:02 +0200

    updated calc.py

--- src/arbmaster/calc.py
+++ src/arbmaster/calc.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 
 class ArbitrageCalc:
     
-    def __init__(self, odds: Dict[str, list], stake: float = 100) -> None:
+    def __init__(self, odds: Dict[str, list], stake: float = 100) -> None:        
         self.odds = [v[0]["price"] for v in odds.values() if len(v) >= 1]
         self.probability: float = self._calc_prob(self.odds)
         
@@ -12,29 +12,32 @@ class ArbitrageCalc:
         self.roi: str = self._calc_roi()
     
     def is_arbitrage(self) -> bool:
-        return self.probability < 1
+        return 0 < self.probability < 1
     
     def _calc_prob(self, odds) -> float:
-        p = 0
+        p = 0.0
         for odd in odds:
-            p += 1 / odd
+            if odd is not None:
+                p += 1 / odd
         return p
     
     def _calc_stakes(self, stake: float) -> List[float]:
         stakes: List[float] = []
         for odd in self.odds:
-            bet = (stake * (1 / odd)) / self.probability
-            stakes.append(bet)
+            if self.is_arbitrage():
+                bet = (stake * (1 / odd)) / self.probability
+                stakes.append(bet)
         return stakes
     
     def _calc_payout(self) -> float:
-        payout: float = 0
-        for odd, stake in zip(self.odds, self.stakes):
-            payout = stake * odd
+        payout: float = 0.0
+        if self.is_arbitrage():
+            for odd, stake in zip(self.odds, self.stakes):
+                payout = stake * odd
         return round(payout, 2)
     
     def _calc_profit(self) -> float:
-        return round(self.payout - sum(self.stakes), 2)
+        return round(self.payout - sum(self.stakes), 2) if self.is_arbitrage() else 0.0
     
     def _calc_roi(self) -> str:
-        return "{:.2f}%".format((self.profit / sum(self.stakes)) * 100)
+        return "{:.2f}%".format((self.profit / sum(self.stakes)) * 100) if self.is_arbitrage() else "0.0%"
